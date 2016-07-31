2709069c79cc3ef23f8208c6efaf822c39488ec2
alexey-lukyanenko <a.lukyanenko@gmail.com>
2016-08-01 00:09:27 +0300

    Customer was renamed to User; ApplicationContextAware was implemented instead of loading context manually

--- src/main/java/com/intetics/lukyanenko/dao/jdbc/JDBCDAOFactory.java
+++ src/main/java/com/intetics/lukyanenko/dao/jdbc/JDBCDAOFactory.java
@@ -3,25 +3,34 @@ package com.intetics.lukyanenko.dao.jdbc;
 import com.intetics.lukyanenko.dao.CommonDAO;
 import com.intetics.lukyanenko.dao.DAOFactory;
 import com.intetics.lukyanenko.models.*;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-public class JDBCDAOFactory implements DAOFactory
+public class JDBCDAOFactory implements DAOFactory, ApplicationContextAware
 {
+  private ApplicationContext context;
+  
   public <T extends Common> CommonDAO<T> getDAO(Class<T> modelClass)
   {
-    ApplicationContext context = new FileSystemXmlApplicationContext("src\\main\\dispatcher-servlet.xml");
     if (modelClass == Order.class)
       return (CommonDAO<T>)context.getBean(OrderDAOImpl.class);
     else if (modelClass == OrderDetail.class)
       return (CommonDAO<T>)context.getBean(OrderDetailDAOImpl.class);
-    else if (modelClass == Customer.class)
-      return (CommonDAO<T>)context.getBean(CustomerDAOImpl.class);
+    else if (modelClass == User.class)
+      return (CommonDAO<T>)context.getBean(UserDAOImpl.class);
     else if (modelClass == GoodsCategory.class)
       return (CommonDAO<T>)context.getBean(GoodsCategoryDAOImpl.class);
     else if (modelClass == GoodsItem.class)
       return (CommonDAO<T>)context.getBean(GoodsItemDAOImpl.class);
     else
       throw new IllegalArgumentException(String.format("Model class %s is not supported", modelClass.getSimpleName()));
+  }
+  
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+  {
+     context = applicationContext;
   }
 }
