6e67c7fa969abd89588245b53da972f0bfbbe5a8
Alexey Lukyanenko <a.lukyanenko@intetics.com>
2016-08-08 15:47:06 +0300

    Binding of item's categories was added

--- src/main/java/com/intetics/lukyanenko/web/GoodsItemController.java
+++ src/main/java/com/intetics/lukyanenko/web/GoodsItemController.java
@@ -5,11 +5,12 @@ import com.intetics.lukyanenko.models.GoodsItem;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
@@ -95,31 +96,75 @@ public class GoodsItemController
   }
   
   @RequestMapping(value = "/{id}", method = RequestMethod.POST)
-  public String update(@ModelAttribute GoodsItem goodsItem)
+  public String update(@ModelAttribute GoodsItem goodsItem,
+                       BindingResult bindingResult,
+                       @RequestParam Map<String, String> params)
   {
+    try
+    {
+      parseParamsToCategories(params, goodsItem);
+    }
+    catch (Throwable E)
+    {
+      bindingResult.addError(new ObjectError("goodsItem.categories", E.getMessage()));
+    }
     service.setGoodsItem(goodsItem);
     return "redirect:/goods";
   }
   
-  @RequestMapping(method = RequestMethod.PUT)
-  public ModelAndView putNew(GoodsItem goodsItem, Map<String, String> params)
+  void parseParamsToCategories(Map<String, String> source, GoodsItem goodsItem)
   {
-    try
+    if (goodsItem.getCategories() == null)
+      goodsItem.setCategories(new ArrayList<>());
+    //
+    for(Map.Entry<String, String> item : source.entrySet())
+      if (item.getKey().contains("categories/"))
+      {
+        GoodsCategory obj = service.getGoodCategoryEmpty();
+        obj.setId(Integer.parseInt(item.getValue()));
+        goodsItem.getCategories().add(obj);
+      }
+  }
+  
+  @RequestMapping(method = RequestMethod.PUT)
+  public ModelAndView putNew(GoodsItem goodsItem,
+                             BindingResult bindingResult)
+  {
+    if (bindingResult.hasErrors())
     {
-      service.setGoodsItem(goodsItem);
-      return new ModelAndView("redirect:/goods");
-    } catch (DuplicateKeyException Exception)
-    {
-      return new ModelAndView("GoodsItemEdit", "model", goodsItem);
-    }
+      ModelAndView mv = new ModelAndView("GoodsItemEdit");
+      mv.addObject("model", goodsItem);
+      mv.addObject("categories", service.getGoodCategories());
+      return mv;
+    } else
+      try
+      {
+        service.setGoodsItem(goodsItem);
+        return new ModelAndView("redirect:/goods");
+      }
+      catch (DuplicateKeyException Exception)
+      {
+        ModelAndView mv = new ModelAndView("GoodsItemEdit");
+        mv.addObject("model", goodsItem);
+        mv.addObject("categories", service.getGoodCategories());
+        return mv;
+      }
   }
   
   @RequestMapping(params = "new", method = RequestMethod.POST)
-  public ModelAndView insertNew(@ModelAttribute GoodsItem goodsItem, BindingResult result,
-  @RequestParam Map<String, String> params)
+  public ModelAndView insertNew(@ModelAttribute GoodsItem goodsItem,
+                                BindingResult bindingResult,
+                                @RequestParam Map<String, String> params)
   {
-    
-    return putNew(goodsItem, params);
+    try
+    {
+      parseParamsToCategories(params, goodsItem);
+    }
+    catch (Throwable E)
+    {
+      bindingResult.addError(new ObjectError("goodsItem.categories", E.getMessage()));
+    }
+    return putNew(goodsItem, bindingResult);
   }
   
   
