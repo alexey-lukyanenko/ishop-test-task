f8e634a7db37a1dcb2e0b123f0d93121accff62e
Alexey Lukyanenko <a.lukyanenko@intetics.com>
2016-09-16 20:09:02 +0300

    daoFactory is Autowired now

--- src/main/java/com/intetics/lukyanenko/models/Common.java
+++ src/main/java/com/intetics/lukyanenko/models/Common.java
@@ -1,11 +1,13 @@
 package com.intetics.lukyanenko.models;
 
 import com.intetics.lukyanenko.dao.DAOFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Common
 {
   protected DAOFactory daoFactory;
   
+  @Autowired
   public void setDaoFactory(DAOFactory daoFactory)
   {
     this.daoFactory = daoFactory;
